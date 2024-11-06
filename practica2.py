4411d0886b365b35630236e6297ff5015a78ac28
RafaBuenoEspinosa <rafabuenoesp2003@gmail.com>
2024-11-06 19:35:50 +0100

    He hecho el menú del  programa con múltiples letras.

--- practica2.py
+++ practica2.py
@@ -3,6 +3,29 @@ import re
 ###################################################################
 #Funciones útiles                                                 #
 ###################################################################
+#Función TextToNumber: Tranformar una cadena Ascii a letra
+def TextToNumber(texto):
+    
+    texto = texto.upper()  # Convertir el texto a mayúsculas para facilitar el mapeo
+    cadena_numerica = []
+    
+    for char in texto:
+        if 'A' <= char <= 'Z':  # Solo considerar letras
+            var=letter2ascii(char)
+            cadena_numerica.append(var)
+    
+    return cadena_numerica
+
+#def NumberToText(num):
+#    cadena_numerica = []
+#    
+#    
+#    for n in texto:
+#       var=ascii2letter(n)
+#        cadena_numerica.append(var)
+#    
+#    return cadena_numerica
+
 
 
 ###################################################################
@@ -33,3 +56,5 @@ import re
 ########################### Ejercicio 4 ###########################
 
 ############################## Menú ###############################
+texto = input("Introduce el texto llano para cifrar con el cifrado afín: ")
+
